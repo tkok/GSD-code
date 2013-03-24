7cb93abe79f1d447d6d0e4f2395f3090ebb0bed8
Kasper Benjamin Hansen <kbhdk1976@gmail.com>
2013-03-24 09:17:12 +0100

    Servlet now contains a timer (currently 3 seconds) for implementing the getting of json from the simulator

--- if-statement-model/src/dk/itu/kben/gsd/servlet/PolicyEngineServlet.java
+++ if-statement-model/src/dk/itu/kben/gsd/servlet/PolicyEngineServlet.java
@@ -1,16 +1,51 @@
 package dk.itu.kben.gsd.servlet;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class PolicyEngineServlet extends HttpServlet {
+	
+	Thread thread = null;
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		
-		System.out.println("INIT WAS CALLED!");
+		thread = new Thread(new Runnable() {
+			Date getNext() {
+				Calendar calendar = new GregorianCalendar();
+				
+				calendar.add(Calendar.SECOND, 3);
+				
+				return calendar.getTime();
+			}
+			
+			@Override
+			public void run() {
+				while (true) {
+				
+					Date nextTime = getNext();
+					
+					while (new Date().getTime() < nextTime.getTime()) {
+						try {
+							Thread.sleep(500);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+		
+					System.out.println("3 seconds passed.");
+				}
+			}
+		});
+		
+		System.out.println("PolicyEngineServlet is starting...");
+		thread.start();
+		System.out.println("PolicyEngineServlet was started.");
 	}
-	
 }
