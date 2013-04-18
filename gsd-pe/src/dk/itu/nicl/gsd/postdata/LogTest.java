672759feab8743fe176ee691bddc076c2c4e363b
Nicolas Wainach <nw@netstarter.dk>
2013-04-18 21:07:00 +0200

    Added Log Class for evaluation
    Added Log Class for evaluation

---
+++ gsd-pe/src/dk/itu/nicl/gsd/postdata/LogTest.java
@@ -0,0 +1,14 @@
+package dk.itu.nicl.gsd.postdata;
+
+public class LogTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Log.log("Some policy action is logged...");
+		
+	}
+
+}
