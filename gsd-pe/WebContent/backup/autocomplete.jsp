84ea5241c04af49b1d1c2160a48e6f8876952df7
Nicolas Wainach <nw@netstarter.dk>
2013-05-07 21:30:54 +0200

    Autocomplete added
    Autocomplete added

---
+++ gsd-pe/WebContent/backup/autocomplete.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <script type="text/javascript" src="/test/js/autocomplete.js"></script>
+</head>
+<body>
+
+<div class="ui-widget">
+  <label for="if-sensorid">If sensor: </label>
+  <input id="if-sensorid" />
+</div>
+
+
+</body>
+</html>
--- gsd-pe/WebContent/js/autocomplete.js
+++ gsd-pe/WebContent/js/autocomplete.js
@@ -1,16 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-  <script>
-  $(function() {
-    var get_sensors = [
+var get_sensors = [
 "energy-battery-1-level",
 "energy-battery-2-level",
 "energy-battery-3-level",
@@ -426,19 +414,3 @@
 "room-9-light-22-production",
 "room-9-light-22-state"
     ];
-    $( "#if-sensorid" ).autocomplete({
-      source: get_sensors
-    });
-  });
-  </script>
-</head>
-<body>
-
-<div class="ui-widget">
-  <label for="if-sensorid">If sensor: </label>
-  <input id="if-sensorid" />
-</div>
-
-
-</body>
-</html>
