f26495df74e9c51692620b9e7bb3d73a798a6b5a
--replace-all <scas@itu.dk>
2013-05-07 23:54:57 +0200

    Changed SetStatement to include wildcards

--- gsd-pe/src/dk/itu/policyengine/domain/SetStatement.java
+++ gsd-pe/src/dk/itu/policyengine/domain/SetStatement.java
@@ -1,9 +1,14 @@
 package dk.itu.policyengine.domain;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 import dk.itu.policyengine.integration.Connection;
-import dk.itu.policyengine.persistence.SensorValueCache;
+import dk.itu.policyengine.utils.Wildcards;
 
 public class SetStatement implements Statement {
 	private transient final Logger logger = Logger.getLogger(this.getClass());
@@ -21,12 +26,31 @@ public class SetStatement implements Statement {
 	}
 	
 	public void execute() {
-		SensorValueCache.setValue(sensorID, aValue);
-		
-		try {
-			new Connection().setSensorValue(sensorID, aValue.getIntValue());
-		} catch (Exception e) {
-			logger.error(e);
-		} 
+		//SensorValueCache.setValue(sensorID, aValue);
+		if(sensorID.contains("floor") && !sensorID.contains("room")){
+			logger.info("It is a wildcard");
+			try {
+				List<String> sensors = new Wildcards().getSensorListByWildcard(sensorID);
+				for(String s : sensors){
+					new Connection().setSensorValue(s+"-gain", aValue.getIntValue());
+				}
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else{
+			try {
+				new Connection().setSensorValue(sensorID, aValue.getIntValue());
+			} catch (Exception e) {
+				logger.error(e);
+			}
+		}
 	}
 }
